a1d35c6bbe3d527dff1bbd14afcb54919657e103
NachoLibre67 <736939@student.dusd.net>
2025-10-28 14:40:07 -0700

    i messed up

---
+++ P7EscamillaScriptingProject/Assets/Scripts/variablesandfunctions.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class variablesandfunctions : MonoBehaviour
+{
+    //Creating An interger
+    public int myint = 6;
+    // Start is called before the first frame update
+    void Start()
+    {
+    //running my int variable thru the function 
+        myint = MultiplyByTwo(myint);
+        Debug.Log(myint);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+int MultiplyByTwo(int number);
+
+    int result;
+    result = number * 2;
+    return result;
